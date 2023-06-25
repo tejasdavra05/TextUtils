d4a75708292fec649bd8cee1c53d0a8228e3dbb0
Tejas Davra <133627898+tejasdavra05@users.noreply.github.com>
2023-06-25 12:20:56 +0530

    Added build

---
+++ precache-manifest.6759bb8e64bc0e336b0e33a4f38dd9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6da59ea0322ed4855c8",
+    "url": "/static/css/main.db186826.chunk.css"
+  },
+  {
+    "revision": "b6da59ea0322ed4855c8",
+    "url": "/static/js/main.b6da59ea.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "9668e687d3cbb8b43b0b",
+    "url": "/static/js/2.9668e687.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "d0a32c51e8f95022737787c184fd6e27",
+    "url": "/index.html"
+  }
+];
